404b479d37df26c5cdfc7b9fc1601d037f7450c2
Lmaleque <lmale001@campus.goldsmiths.ac.uk>
2025-10-22 13:24:44 +0100

    04_mypage_33801543

---
+++ index.js
@@ -0,0 +1,22 @@
+// Set up express
+const express = require("express");
+const app = express();
+const port = 8000; 
+
+// Handle the routes
+
+//Homepage: This page will just display the phrase "Hello World!"
+app.get("/home", (req, res) => res.send("Hello World!"));
+
+//About page: This page will briefly talk about myself, what I study and where.
+app.get('/about', (req, res) => res.send ('<h1>About page</h1>' + '<p> My name is Lubna Maleque and I am a student at Goldsmiths, University of London, studying BSc Creative computing. </p>' ))
+
+//Conatct: This page has all my relevant contact information.
+app.get('/contact', (req, res) => res.send ('<h1>CONTACT ME</h1>' + '<p1>Primary email: lmale001@gold.ac.uk</p1>' + '<p2> Secondary email: lubnamaleque15@gmail.com</p2>' + '<p3> Phone number: 07521146040</p3>'))
+
+//Date: This page will display the current date and time in your timezone. Using  HTML and javascript's Date object.
+app.get('/date', (req, res) => res.send ('<h1>TODAYS DATE:</h1>' + new Date().toDateString() + '<h2>CURRENT TIME:</h2>' + new Date().toLocaleTimeString() + '<h3> TIMEZONE:</h3>' + Intl.DateTimeFormat().resolvedOptions().timeZone ))
+
+// Start listening for HTTP requests
+app.listen(port, 
+    () => console.log(`Node server is running on port ${port}...`));
